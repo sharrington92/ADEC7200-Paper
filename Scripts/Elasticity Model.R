2d415db18d21719a49239b7f4a01d1f1e7d4926f
Shaun Harrington <shaunharrington17@gmail.com>
2021-04-06 21:31:14 -0700

    Created model with all terms for each region and another that selects best regional variables for total revenue.

--- Scripts/Elasticity Model.R
+++ Scripts/Elasticity Model.R
@@ -1,8 +1,10 @@
 #Libraries
 {
   library(tidyverse)
+  library(broom)
+  library(car)
   
-  option(scipen = 9999)
+  options(scipen = 9999)
 }
 
 #User-defined functions
@@ -48,24 +50,70 @@
 
 #Construct model with respect to region
 {
+  
   indicators %>% 
-    filter(Year < 2019, Year > 2003, Revenue > 0, !is.na(Revenue)) %>% 
-    group_by(Region) %>% 
+    filter(Region == "US", Year > 2002, Year < 2019) %>% 
     lm(
       dlog(Revenue) ~ dlog(consumption) + dlog(gdp) + dlog(indust.production) + dlog(cpi) +
-        diff(interest.nom) + dlog(gas.price) + dlog(oil.price) + diff(unemployment.rate) + 
-        dlog(exchange.rate.index)
-        #as.factor(Region)
-      , data = .
+        diff(interest.nom) + #dlog(gas.price) + 
+        dlog(oil.price) + diff(unemployment.rate) + dlog(exchange.rate.index)
+      , data = ., na.action = na.omit
     ) %>% 
     summary(.)
   
+  #Split data by region, evaluate linear model
   model.regional <- indicators %>% 
     filter(Year < 2019, Year > 2003, Revenue > 0, gas.price > 0) %>% #View()
     nest(-Region) %>% 
-    mutate(fit = map(data, ~ lm(
-      dlog(Revenue) ~ dlog(consumption) + dlog(gdp) + dlog(indust.production) + dlog(cpi) +
-        diff(interest.nom) + dlog(gas.price) + dlog(oil.price) + diff(unemployment.rate) + dlog(exchange.rate.index)
-      , data = .
-    )))
+    mutate(
+      fit = map(data, ~ lm(
+        dlog(Revenue) ~ dlog(consumption) + dlog(gdp) + dlog(indust.production) + dlog(cpi) +
+          diff(interest.nom) + dlog(gas.price) + dlog(oil.price) + diff(unemployment.rate) + dlog(exchange.rate.index)
+        , data = ., na.action = na.omit
+      )),
+      results = map(fit, glance)
+    ) %>% 
+    unnest(results)
 }
+
+#Get only most promising variables by region
+{
+  model.tuned <- indicators %>% 
+    pivot_longer(
+      -c(Year, Region), names_to = "Indicator", values_to = "Value"
+    ) %>% 
+    mutate(ID = paste(Region, Indicator, sep = ".")) %>%
+    ungroup() %>% 
+    select(-c(Indicator, Region)) %>% 
+    pivot_wider(names_from = ID, values_from = Value) %>% 
+    mutate(
+      Revenue.Total = Americas.Other.Revenue + US.Revenue + Europe.Revenue + Asia.Oceania.Africa.Revenue
+    ) %>% 
+    filter(Year < 2019, Year > 2003) %>% 
+    lm(
+      dlog(Revenue.Total) ~ 
+        #dlog(Asia.Oceania.Africa.oil.price) +
+        dlog(Europe.gdp /  Europe.population) +
+        #dlog(US.indust.production) +
+        dlog(US.consumption / US.population) +
+        #dlog(US.gdp / US.population) +
+        dlog(Europe.oil.price) +
+        #dlog(Europe.exchange.rate.index) +
+        #diff(US.interest.nom) +
+        #dlog(Americas.Other.consumption / Americas.Other.population) +
+        diff(Asia.Oceania.Africa.exchange.rate.index) +
+        dlog(Asia.Oceania.Africa.consumption / Asia.Oceania.Africa.population) 
+      , data = ., na.action = na.omit
+    )
+  
+  summary(model.tuned)
+  
+  #Test for multicollinearity
+  vif(model.tuned)
+}
+# "Region"              "Year"                "consumption"        
+# [4] "gdp"                 "indust.production"   "cpi"                
+# [7] "interest.nom"        "gas.price"           "oil.price"          
+# [10] "unemployment.rate"   "population"          "exchange.rate.index"
+# [13] "Revenue"            
+
